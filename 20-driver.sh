11c82987a89343490ee6a22f582a1d1f32a6fb7d
JAEHWAKIM <comnikist@hotmail.com>
2026-07-01 10:48:53 +0900

    Update 20-driver.sh

--- 20-driver.sh
+++ 20-driver.sh
@@ -3,20 +3,20 @@
 #20-driver.sh
 cd "$(dirname "$0")"
 
+DRIVER_VERSION="1.1.7"
+DRIVER_FILENAME="libftd3xx-linux-arm-v8-${DRIVER_VERSION}.tgz"
+
 if [ ! -d "./temp" ]; then
     mkdir ./temp
 fi
 
 cd ./temp
 
-#다운로드
-if [ ! -f "libftd3xx-linux-arm-v8-1.0.16.tgz" ]; then
-    wget https://ftdichip.com/wp-content/uploads/2024/07/libftd3xx-linux-arm-v8-1.0.16.tgz
-fi
+mv "../${DRIVER_FILENAME}" .
 
 #압축해제
 if [ ! -d "linux-arm-v8" ]; then
-tar -xvzf libftd3xx-linux-arm-v8-1.0.16.tgz
+tar -xvzf ${DRIVER_FILENAME}
 fi
 
 cd ./linux-arm-v8
@@ -26,6 +26,6 @@ if [ -f "/usr/lib/libftd3xx.so" ]; then
     sudo rm -v /usr/lib/libftd3xx.so
 fi
 sudo cp -v libftd3xx.so /usr/lib/
-sudo cp -v libftd3xx.so.1.0.16 /usr/lib/
+sudo cp -v libftd3xx.so.${DRIVER_VERSION} /usr/lib/
 sudo cp -v 51-ftd3xx.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
